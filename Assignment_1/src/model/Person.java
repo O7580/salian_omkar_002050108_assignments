cc1e2af06cdfb5f8e9d48a8dd8c9a1d447d4c4a3
Omkar Salian <salian.o@northeastern.edu>
2024-09-17 23:33:11 -0400

    Assignment_1_Person_Profile

---
+++ Assignment_1/src/model/Person.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author omkarsalian
+ */
+public class Person {
+    String firstname;
+    String middlename;
+    String lastname;
+    String email;
+    String phone;
+    String driverslicensenumber;
+    String driverslicensestate;
+    String socialsecuritynumber;
+    String addressline1;
+    String addressline2;
+    String city;
+    String state;
+    String zip;
+    String telephonenumber;
+    String faxnumber;
+    String gender;
+    String maritalstatus;
+    String nationality;
+    String aadharcardnumber;
+    String pancardnumber;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDriverslicensenumber() {
+        return driverslicensenumber;
+    }
+
+    public void setDriverslicensenumber(String driverslicensenumber) {
+        this.driverslicensenumber = driverslicensenumber;
+    }
+
+    public String getDriverslicensestate() {
+        return driverslicensestate;
+    }
+
+    public void setDriverslicensestate(String driverslicensestate) {
+        this.driverslicensestate = driverslicensestate;
+    }
+
+    public String getSocialsecuritynumber() {
+        return socialsecuritynumber;
+    }
+
+    public void setSocialsecuritynumber(String socialsecuritynumber) {
+        this.socialsecuritynumber = socialsecuritynumber;
+    }
+
+    public String getAddressline1() {
+        return addressline1;
+    }
+
+    public void setAddressline1(String addressline1) {
+        this.addressline1 = addressline1;
+    }
+
+    public String getAddressline2() {
+        return addressline2;
+    }
+
+    public void setAddressline2(String addressline2) {
+        this.addressline2 = addressline2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTelephonenumber() {
+        return telephonenumber;
+    }
+
+    public void setTelephonenumber(String telephonenumber) {
+        this.telephonenumber = telephonenumber;
+    }
+
+    public String getFaxnumber() {
+        return faxnumber;
+    }
+
+    public void setFaxnumber(String faxnumber) {
+        this.faxnumber = faxnumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMaritalstatus() {
+        return maritalstatus;
+    }
+
+    public void setMaritalstatus(String maritalstatus) {
+        this.maritalstatus = maritalstatus;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getAadharcardnumber() {
+        return aadharcardnumber;
+    }
+
+    public void setAadharcardnumber(String aadharcardnumber) {
+        this.aadharcardnumber = aadharcardnumber;
+    }
+
+    public String getPancardnumber() {
+        return pancardnumber;
+    }
+
+    public void setPancardnumber(String pancardnumber) {
+        this.pancardnumber = pancardnumber;
+    }
+
+
+}
